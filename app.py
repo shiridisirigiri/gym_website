8d3c211b7bf7eb275b7554368c5fb5f2c852d2bc
shiridisirigiri <shiridisirigiri9369@gmail.com>
2025-07-16 10:53:41 +0530

    Use environment PORT for Render deployment

--- app.py
+++ app.py
@@ -124,4 +124,5 @@ def thankyou():
 
 if __name__ == '__main__':
     init_db()
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))  # Use PORT from env or default 5000
+    app.run(host="0.0.0.0", port=port)
